9e5518e7a0c25a95849f1fef44a2dee8928c7385
kiradpro <kiradpro@users.noreply.github.com>
2022-05-03 01:42:38 +0200

    add mockapi.io

--- src/App.js
+++ src/App.js
@@ -21,9 +21,9 @@ function App() {
     async function fetchData() {
       try {
         const [cartResponse, favoritesResponse, itemsResponse] = await Promise.all([
-          axios.get('https://60d62397943aa60017768e77.mockapi.io/cart'),
-          axios.get('https://60d62397943aa60017768e77.mockapi.io/favorites'),
-          axios.get('https://60d62397943aa60017768e77.mockapi.io/items'),
+          axios.get('https://6270606b6a36d4d62c187306.mockapi.io/cart'),
+          axios.get('https://6270606b6a36d4d62c187306.mockapi.io/favorites'),
+          axios.get('https://6270606b6a36d4d62c187306.mockapi.io/items'),
         ]);
 
         setIsLoading(false);
@@ -44,10 +44,10 @@ function App() {
       const findItem = cartItems.find((item) => Number(item.parentId) === Number(obj.id));
       if (findItem) {
         setCartItems((prev) => prev.filter((item) => Number(item.parentId) !== Number(obj.id)));
-        await axios.delete(`https://60d62397943aa60017768e77.mockapi.io/cart/${findItem.id}`);
+        await axios.delete(`https://6270606b6a36d4d62c187306.mockapi.io/cart/${findItem.id}`);
       } else {
         setCartItems((prev) => [...prev, obj]);
-        const { data } = await axios.post('https://60d62397943aa60017768e77.mockapi.io/cart', obj);
+        const { data } = await axios.post('https://6270606b6a36d4d62c187306.mockapi.io/cart', obj);
         setCartItems((prev) =>
           prev.map((item) => {
             if (item.parentId === data.parentId) {
@@ -68,7 +68,7 @@ function App() {
 
   const onRemoveItem = (id) => {
     try {
-      axios.delete(`https://60d62397943aa60017768e77.mockapi.io/cart/${id}`);
+      axios.delete(`https://6270606b6a36d4d62c187306.mockapi.io/cart/${id}`);
       setCartItems((prev) => prev.filter((item) => Number(item.id) !== Number(id)));
     } catch (error) {
       alert('Ошибка при удалении из корзины');
@@ -79,11 +79,11 @@ function App() {
   const onAddToFavorite = async (obj) => {
     try {
       if (favorites.find((favObj) => Number(favObj.id) === Number(obj.id))) {
-        axios.delete(`https://60d62397943aa60017768e77.mockapi.io/favorites/${obj.id}`);
+        axios.delete(`https://6270606b6a36d4d62c187306.mockapi.io/favorites/${obj.id}`);
         setFavorites((prev) => prev.filter((item) => Number(item.id) !== Number(obj.id)));
       } else {
         const { data } = await axios.post(
-          'https://60d62397943aa60017768e77.mockapi.io/favorites',
+          'https://6270606b6a36d4d62c187306.mockapi.io/favorites',
           obj,
         );
         setFavorites((prev) => [...prev, data]);
